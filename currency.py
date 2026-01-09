810a5ed8b23524d308d3e94109bf3c8a4b39eb64
ArifGS <arifgs901@gmail.com>
2026-01-09 22:34:54 +0800

    Adding currency.py

---
+++ currency.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+
+CO = int(input("What do you have left in pesos? "))
+PE = int(input("What do you have left in pesos? "))
+BR = int(input("What do you have left in pesos? "))
+
+US = (CO*0.00027) + (PE*0.30) + (BR*0.19)
+print(US)
