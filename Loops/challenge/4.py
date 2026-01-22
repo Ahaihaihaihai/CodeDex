f62a7962a3b2415b628335560f2bbd407ecdfb9f
ArifGS <arifgs901@gmail.com>
2026-01-22 19:58:22 +0800

    add challenge

---
+++ Loops/challenge/4.py
@@ -0,0 +1,6 @@
+# for i in range(1,25):
+#     for j in range(i):
+#         print('* ',end="")
+#     print('')
+for i in range(1,25):
+    print('* ' * i)
---
+++ Loops/challenge/5.py
@@ -0,0 +1,8 @@
+x = int(input())
+
+total = 0
+
+for i in range(x+1):
+    total += i*i
+
+print(total)
