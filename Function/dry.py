b017571d38d7a95ba80bb5707c47c2891a200752
ArifGS <arifgs901@gmail.com>
2026-01-23 19:33:33 +0800

    Add dry.py

---
+++ Function/dry.py
@@ -0,0 +1,13 @@
+# print() - to show the output in the terminal
+print('Hello World')
+
+# input() - to get input from user
+Name = input('What\'s Your Name?: ')
+
+# len() - to count the length of string
+print('Length of your name = ' + str(len(Name)))
+
+# abs() - to absolute a value
+x = -5
+print(x)
+print('x after abs(x): ' + str(abs(x)))
