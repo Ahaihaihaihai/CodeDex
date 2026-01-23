4424c2f1b3a0f78f1aff968702360a3543622275
ArifGS <arifgs901@gmail.com>
2026-01-23 19:21:57 +0800

    Challenge finished

--- List/challenge/1.py
+++ List/challenge/1.py
@@ -1,15 +1,5 @@
-best_pictures = [
-  '2019 - Parasite',
-  '2018 - Green Book',
-  '2017 - The Shape of Water',
-  '2016 - Moonlight',
-  '2015 - Spotlight',
-  '2014 - Birdman',
-  '2013 - 12 Years a Slave',
-  '2012 - Argo',
-  '2011 - The Artist'
-]
+wishlist = ['Nvme M.2 Samsung 990 Pro 1TB',
+            'IPhone 17 Pro Max 512Gb',
+            'Asus ROG RTX 4090Ti']
 
-best_pictures.extend(['2020 - Nomadland', '2021 - CODA', '2022 - Everything Everywhere All at Once', '2023 - Oppenheimer', '2024 - Anora'])
-best_pictures.sort(reverse=True)
-print(best_pictures)
+print(wishlist)
---
+++ List/challenge/3.py
@@ -0,0 +1,15 @@
+best_pictures = [
+  '2019 - Parasite',
+  '2018 - Green Book',
+  '2017 - The Shape of Water',
+  '2016 - Moonlight',
+  '2015 - Spotlight',
+  '2014 - Birdman',
+  '2013 - 12 Years a Slave',
+  '2012 - Argo',
+  '2011 - The Artist'
+]
+
+best_pictures.extend(['2020 - Nomadland', '2021 - CODA', '2022 - Everything Everywhere All at Once', '2023 - Oppenheimer', '2024 - Anora'])
+best_pictures.sort(reverse=True)
+print(best_pictures)
